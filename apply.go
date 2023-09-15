f340606f9a89ec1e3f30b53e135a43a68ac682e4
Xkonti <xkonti@xkonti.tech>
2023-09-14 23:50:09 -0400

    Expose Apply operation and add documentation to it

--- apply.go
+++ apply.go
@@ -1,17 +1,25 @@
 package govec
 
-func (v V2F[T]) apply(f func(T) T) V2F[T] {
-	return V2F[T]{X: f(v.X), Y: f(v.Y)}
+// Apply applies a function to each component of a vector.
+// The function passed to Apply takes the component and the index of the component as arguments.
+func (v V2F[T]) Apply(f func(T, int8) T) V2F[T] {
+	return V2F[T]{X: f(v.X, 0), Y: f(v.Y, 1)}
 }
 
-func (v V3F[T]) apply(f func(T) T) V3F[T] {
-	return V3F[T]{X: f(v.X), Y: f(v.Y), Z: f(v.Z)}
+// Apply applies a function to each component of a vector.
+// The function passed to Apply takes the component and the index of the component as arguments.
+func (v V3F[T]) Apply(f func(T, int8) T) V3F[T] {
+	return V3F[T]{X: f(v.X, 0), Y: f(v.Y, 1), Z: f(v.Z, 2)}
 }
 
-func (v V2I[T]) apply(f func(T) T) V2I[T] {
-	return V2I[T]{X: f(v.X), Y: f(v.Y)}
+// Apply applies a function to each component of a vector.
+// The function passed to Apply takes the component and the index of the component as arguments.
+func (v V2I[T]) Apply(f func(T, int8) T) V2I[T] {
+	return V2I[T]{X: f(v.X, 0), Y: f(v.Y, 1)}
 }
 
-func (v V3I[T]) apply(f func(T) T) V3I[T] {
-	return V3I[T]{X: f(v.X), Y: f(v.Y), Z: f(v.Z)}
+// Apply applies a function to each component of a vector.
+// The function passed to Apply takes the component and the index of the component as arguments.
+func (v V3I[T]) Apply(f func(T, int8) T) V3I[T] {
+	return V3I[T]{X: f(v.X, 0), Y: f(v.Y, 1), Z: f(v.Z, 2)}
 }
