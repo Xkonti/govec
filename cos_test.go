82f38e3b03382d44a541612557c9da6b59e0943d
Karolos Lykos <Karolos.Lykos@powerfactors.com>
2023-10-07 19:36:28 +0300

    mod(cos_test): add more test cases

--- cos_test.go
+++ cos_test.go
@@ -6,37 +6,37 @@ import (
 )
 
 func TestV2F_Cos(t *testing.T) {
-	v1 := V2F[float64]{X: 0, Y: math.Pi / 2}
+	v1 := V2F[float64]{X: 0, Y: math.Pi}
 	v2 := v1.Cos()
 
-	if v2.X != 0 || v2.Y != 1 {
+	if v2.X != 1 || v2.Y != -1 {
 		t.Errorf("V2F Cos failed")
 	}
 }
 
 func TestV2F_CosInPlace(t *testing.T) {
-	v1 := V2F[float64]{X: 0, Y: math.Pi / 2}
+	v1 := V2F[float64]{X: 0, Y: math.Pi}
 	v1.CosInPlace()
 
-	if v1.X != 0 || v1.Y != 1 {
+	if v1.X != 1 || v1.Y != -1 {
 		t.Errorf("V2F CosInPlace failed")
 	}
 }
 
 func TestV3F_Cos(t *testing.T) {
-	v1 := V3F[float64]{X: 0, Y: math.Pi / 2, Z: 3 * math.Pi / 2}
+	v1 := V3F[float32]{X: 0, Y: math.Pi, Z: 2 * math.Pi}
 	v2 := v1.Cos()
 
-	if v2.X != 0 || v2.Y != 1 || v2.Z != -1 {
+	if v2.X != 1 || v2.Y != -1 || v2.Z != 1 {
 		t.Errorf("V3F Cos failed")
 	}
 }
 
 func TestV3F_CosInPlace(t *testing.T) {
-	v1 := V3F[float64]{X: 0, Y: math.Pi / 2, Z: 3 * math.Pi / 2}
+	v1 := V3F[float64]{X: 0, Y: math.Pi, Z: 2 * math.Pi}
 	v1.CosInPlace()
 
-	if v1.X != 0 || v1.Y != 1 || v1.Z != -1 {
+	if v1.X != 1 || v1.Y != -1 || v1.Z != 1 {
 		t.Errorf("V3F CosInPlace failed")
 	}
 }
@@ -45,7 +45,7 @@ func TestV2I_Cos(t *testing.T) {
 	v1 := V2I[int16]{X: 0, Y: 1}
 	v2 := v1.Cos()
 
-	if v2.X != 0 || v2.Y != 0.8414709848078965 {
+	if v2.X != 1 || v2.Y != 0.5403023058681398 {
 		t.Errorf("V2F Cos failed")
 	}
 }
@@ -54,7 +54,7 @@ func TestV3I_Cos(t *testing.T) {
 	v1 := V3I[int16]{X: 0, Y: 1, Z: 3}
 	v2 := v1.Cos()
 
-	if v2.X != 0 || v2.Y != 0.8414709848078965 || v2.Z != 0.1411200080598672 {
+	if v2.X != 1 || v2.Y != 0.5403023058681398 || v2.Z != -0.9899924966004454 {
 		t.Errorf("V3I Cos failed")
 	}
 }
