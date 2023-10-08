3f094fe35a237987a6e51bed90a9eb11a19dd62e
Karolos Lykos <Karolos.Lykos@powerfactors.com>
2023-10-08 13:56:14 +0300

    test(tan): fix test cases

--- tan_test.go
+++ tan_test.go
@@ -9,7 +9,7 @@ func TestV2F_Tan(t *testing.T) {
 	v1 := V2F[float64]{X: 0, Y: math.Pi / 4}
 	v2 := v1.Tan()
 
-	if v2.X != 0 || !almostEqual(v2.Y, 1, 1e-10) {
+	if !almostEqual(v2.X, 0, 1e-10) || !almostEqual(v2.Y, 1, 1e-10) {
 		t.Errorf("V2F Tan failed")
 	}
 }
@@ -18,7 +18,7 @@ func TestV2F_TanInPlace(t *testing.T) {
 	v1 := V2F[float64]{X: 0, Y: math.Pi / 4}
 	v1.TanInPlace()
 
-	if v1.X != 0 || !almostEqual(v1.Y, 1, 1e-10) {
+	if !almostEqual(v1.Y, 1, 1e-10) || !almostEqual(v1.Y, 1, 1e-10) {
 		t.Errorf("V2F TanInPlace failed")
 	}
 }
@@ -27,7 +27,7 @@ func TestV3F_Tan(t *testing.T) {
 	v1 := V3F[float64]{X: 0, Y: math.Pi / 4, Z: math.Pi}
 	v2 := v1.Tan()
 
-	if v2.X != 0 || !almostEqual(v2.Y, 1, 1e-10) || !almostEqual(v2.Z, 0, 1e-10) {
+	if !almostEqual(v2.X, 0, 1e-10) || !almostEqual(v2.Y, 1, 1e-10) || !almostEqual(v2.Z, 0, 1e-10) {
 		t.Errorf("V3F Tan failed")
 	}
 }
@@ -36,7 +36,7 @@ func TestV3F_TanInPlace(t *testing.T) {
 	v1 := V3F[float64]{X: 0, Y: math.Pi / 4, Z: math.Pi}
 	v1.TanInPlace()
 
-	if v1.X != 0 || !almostEqual(v1.Y, 1, 1e-10) || !almostEqual(v1.Z, 0, 1e-10) {
+	if !almostEqual(v1.Y, 1, 1e-10) || !almostEqual(v1.Y, 1, 1e-10) || !almostEqual(v1.Z, 0, 1e-10) {
 		t.Errorf("V3F TanInPlace failed")
 	}
 }
@@ -45,7 +45,7 @@ func TestV2I_Tan(t *testing.T) {
 	v1 := V2I[int16]{X: 0, Y: 1}
 	v2 := v1.Tan()
 
-	if v2.X != 0 || v2.Y != 1.557407724654902 {
+	if !almostEqual(v2.X, 0, 1e-10) || !almostEqual(v2.Y, math.Tan(1), 1e-10) {
 		t.Errorf("V2I Tan failed")
 	}
 }
@@ -54,7 +54,7 @@ func TestV3I_Tan(t *testing.T) {
 	v1 := V3I[int16]{X: 0, Y: 1, Z: 3}
 	v2 := v1.Tan()
 
-	if v2.X != 0 || v2.Y != 1.557407724654902 || v2.Z != -0.1425465430742778 {
+	if !almostEqual(v2.X, 0, 1e-10) || !almostEqual(v2.Y, math.Tan(1), 1e-10) || !almostEqual(v2.Z, math.Tan(3), 1e-10) {
 		t.Errorf("V3I Tan failed")
 	}
 }
