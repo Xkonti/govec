c66e49dfb5937e4aacc55686417d9b9bf95e6661
Karolos Lykos <Karolos.Lykos@powerfactors.com>
2023-10-08 14:04:17 +0300

    test(sin): fix test cases

--- sin_test.go
+++ sin_test.go
@@ -9,7 +9,7 @@ func TestV2F_Sin(t *testing.T) {
 	v1 := V2F[float64]{X: 0, Y: math.Pi / 2}
 	v2 := v1.Sin()
 
-	if v2.X != 0 || v2.Y != 1 {
+	if !almostEqual(v2.X, 0, 1e-10) || !almostEqual(v2.Y, 1, 1e-10) {
 		t.Errorf("V2F Sin failed")
 	}
 }
@@ -18,7 +18,7 @@ func TestV2F_SinInPlace(t *testing.T) {
 	v1 := V2F[float64]{X: 0, Y: math.Pi / 2}
 	v1.SinInPlace()
 
-	if v1.X != 0 || v1.Y != 1 {
+	if !almostEqual(v1.X, 0, 1e-10) || !almostEqual(v1.Y, 1, 1e-10) {
 		t.Errorf("V2F SinInPlace failed")
 	}
 }
@@ -27,7 +27,7 @@ func TestV3F_Sin(t *testing.T) {
 	v1 := V3F[float64]{X: 0, Y: math.Pi / 2, Z: 3 * math.Pi / 2}
 	v2 := v1.Sin()
 
-	if v2.X != 0 || v2.Y != 1 || v2.Z != -1 {
+	if !almostEqual(v2.X, 0, 1e-10) || !almostEqual(v2.Y, 1, 1e-10) || !almostEqual(v2.Z, -1, 1e-10) {
 		t.Errorf("V3F Sin failed")
 	}
 }
@@ -36,7 +36,7 @@ func TestV3F_SinInPlace(t *testing.T) {
 	v1 := V3F[float64]{X: 0, Y: math.Pi / 2, Z: 3 * math.Pi / 2}
 	v1.SinInPlace()
 
-	if v1.X != 0 || v1.Y != 1 || v1.Z != -1 {
+	if !almostEqual(v1.X, 0, 1e-10) || !almostEqual(v1.Y, 1, 1e-10) || !almostEqual(v1.Z, -1, 1e-10) {
 		t.Errorf("V3F SinInPlace failed")
 	}
 }
@@ -45,7 +45,7 @@ func TestV2I_Sin(t *testing.T) {
 	v1 := V2I[int16]{X: 0, Y: 1}
 	v2 := v1.Sin()
 
-	if v2.X != 0 || v2.Y != 0.8414709848078965 {
+	if !almostEqual(v2.X, 0, 1e-10) || !almostEqual(v2.Y, math.Sin(1), 1e-10) {
 		t.Errorf("V2I Sin failed")
 	}
 }
@@ -54,7 +54,7 @@ func TestV3I_Sin(t *testing.T) {
 	v1 := V3I[int16]{X: 0, Y: 1, Z: 3}
 	v2 := v1.Sin()
 
-	if v2.X != 0 || v2.Y != 0.8414709848078965 || v2.Z != 0.1411200080598672 {
+	if !almostEqual(v2.X, 0, 1e-10) || !almostEqual(v2.Y, math.Sin(1), 1e-10) || !almostEqual(v2.Z, math.Sin(3), 1e-10) {
 		t.Errorf("V3I Sin failed")
 	}
 }
