98488a186e2a10b27fd4bc545a133b70b5f62472
Wu Tingfeng <wutingfeng@outlook.com>
2023-10-09 14:43:58 +0800

    V2F and V3F to return T value.

--- average.go
+++ average.go
@@ -2,16 +2,16 @@ package govec
 
 // V2F
 
-// Average calculates the average of all components of the vector and returns a float64 value.
-func (v V2F[T]) Average() float64 {
-	return (float64(v.X) + float64(v.Y)) / 2
+// Average calculates the average of all components of the vector and returns a T value.
+func (v V2F[T]) Average() T {
+	return (v.X + v.Y) / 2
 }
 
 // V3F
 
-// Average calculates the average of all components of the vector and returns a float64 value.
-func (v V3F[T]) Average() float64 {
-	return (float64(v.X) + float64(v.Y) + float64(v.Z)) / 3
+// Average calculates the average of all components of the vector and returns a T value.
+func (v V3F[T]) Average() T {
+	return (v.X + v.Y + v.Z) / 3
 }
 
 // V2I
