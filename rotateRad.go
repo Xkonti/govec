343ecc912b0a2b0c3e6b649c4b11aa74fc97dac5
Sathvik Hebbar <sathu.hebbar@gmail.com>
2023-10-10 21:36:39 +0530

    Refactoring return types and comments

--- rotateRad.go
+++ rotateRad.go
@@ -5,17 +5,17 @@ import (
 )
 
 // RotateRad returns the vector obtained by rotating
-// the specified 2D vector anticlockwise by the angle
+// the specified 2D vector counterclockwise by the angle
 // specified in radians.
-func (v V2F[T]) RotateRad(angleRad float64) V2F[float64] {
-	return V2F[float64]{
-		X: float64(v.X)*math.Cos(angleRad) - float64(v.Y)*math.Sin(angleRad),
-		Y: float64(v.X)*math.Sin(angleRad) + float64(v.Y)*math.Cos(angleRad),
+func (v V2F[T]) RotateRad(angleRad float64) V2F[T] {
+	return V2F[T]{
+		X: T(float64(v.X)*math.Cos(angleRad) - float64(v.Y)*math.Sin(angleRad)),
+		Y: T(float64(v.X)*math.Sin(angleRad) + float64(v.Y)*math.Cos(angleRad)),
 	}
 }
 
 // RotateRad returns the vector obtained by rotating
-// the specified 2D vector anticlockwise by the angle
+// the specified 2D vector counterclockwise by the angle
 // specified in radians.
 func (v V2I[T]) RotateRad(angleRad float64) V2F[float64] {
 	return V2F[float64]{
